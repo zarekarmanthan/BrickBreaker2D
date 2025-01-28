d282421fb9b99730169191b21c4bb937dbc2bfa6
zarekarmanthan <76549770+zarekarmanthan@users.noreply.github.com>
2025-01-28 09:48:57 +0530

    Keyboard Arrows Keys for Paddle Movement

--- Assets/Scripts/PaddleHandler.cs
+++ Assets/Scripts/PaddleHandler.cs
@@ -5,6 +5,8 @@ public class PaddleHandler : MonoBehaviour
 {
     public Slider slider; // Assign the slider in the Inspector
     public float boundary = 2.5f; // Horizontal boundary for the paddle
+    
+    float sliderSpeed = 0.1f;
 
     public Vector2 starPos;
     void Start()
@@ -23,5 +25,23 @@ public class PaddleHandler : MonoBehaviour
         Vector3 position = transform.position;
         position.x = slider.value; // Set the paddle's x-position to match the slider
         transform.position = position;
+
+        HandleKeyboardInput();
+    }
+
+
+    void HandleKeyboardInput()
+    {
+        // Check for left and right arrow keys
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            slider.value -= sliderSpeed * Time.deltaTime * 70; // Move slider to the left
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            slider.value += sliderSpeed * Time.deltaTime * 70; // Move slider to the right
+        }
+
+        slider.value = Mathf.Clamp(slider.value, slider.minValue, slider.maxValue);
     }
 }
